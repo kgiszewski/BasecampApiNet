a73c67891cf35e27cd2fffe5110f948fe33635ee
kgiszewski <kevin@giszewski.com>
2015-09-04 10:12:30 -0400

    Add a real unit test

---
+++ src/BasecampApiNetUnitTests/CoreTests/CoreTest.cs
@@ -0,0 +1,22 @@
+﻿using BasecampApiNet.Core;
+using BasecampApiNetUnitTests.Models;
+using NUnit.Framework;
+
+namespace BasecampApiNetUnitTests.CoreTests
+{
+    [TestFixture]
+    public class CoreTest
+    {
+        [Test]
+        public void Can_Cache_Responses()
+        {
+            var credentials = CredentialModel.GetCredentials();
+
+            var api = new BasecampApiFactory().GetApi(credentials.AccountId, credentials.Username, credentials.Password);
+
+            var people = api.People.GetAll();
+
+            Assert.IsNotNullOrEmpty(api.CacheDump());
+        }
+    }
+}
---
+++ src/BasecampApiNetUnitTests/Models/CredentialModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+
+namespace BasecampApiNetUnitTests.Models
+{
+    public class CredentialModel
+    {
+        public string AccountId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public static CredentialModel GetCredentials()
+        {
+            var doc = XDocument.Load("BasecampTestCredentials.xml");
+
+            return new CredentialModel()
+            {
+                AccountId = doc.Root.Element("accountId").Value,
+                Username = doc.Root.Element("username").Value,
+                Password = doc.Root.Element("password").Value
+            };
+        }
+    }
+}
